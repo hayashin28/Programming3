51abbf13c0fb55a58116571be3034ca377f4be62
hayashin28 <hayashin.28@gmail.com>
2017-06-18 22:51:40 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson03/Volumes.java
@@ -0,0 +1,53 @@
+package lesson03;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+/**
+ * 第3回 最終課題
+ * @author 林宏典（15T055）
+ *
+ */
+public class Volumes extends Applet {
+
+	@Override
+	public void init(){
+	}
+
+	@Override
+	public void paint(Graphics g) {
+	}
+
+
+
+
+
+
+
+	class MyVolume {
+
+		private int to;		// 斗を記憶する変数
+		private int syou;	// 升を記憶する変数
+		private int gou;	// 合を記憶する変数
+
+		// 体積を設定するメソッド
+		public void setVolume (int t, int s, int g) {
+		}
+
+		// toを参照するメソッド
+		public int getTo () {return to; }
+
+		// syouを参照するメソッド
+		public int getSyou () { return syou; }
+
+		// gouを参照するメソッド
+		public int getGou (){ return gou; }
+
+		// 体積の和を求めるメソッド
+		public MyVolume add(MyVolume x) {
+			return null;
+		}
+
+	}
+
+}
