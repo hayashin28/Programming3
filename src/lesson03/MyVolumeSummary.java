3a9c237efd46e87e6eb60c8b6d4d7941e024d98b
Hayashin28 <hayashin.28@gmail.com>
2017-06-19 12:13:50 +0900

    Add files via upload

--- src/lesson03/MyVolumeSummary.java
+++ src/lesson03/MyVolumeSummary.java
@@ -12,12 +12,14 @@ public class MyVolumeSummary extends Applet {
 	@Override
 	public void init() {
 
-		int to[]   = { 2, 4, 5, 1, 3 };	//
-		int syou[] = { 2, 3, 5, 1, 4 };	//
-		int gou[]  = { 4, 2, 3, 1, 5 };	//
+		int to[]   = { 2, 4, 5, 1, 3 };	// 斗
+		int syou[] = { 2, 3, 5, 1, 4 };	// 升
+		int gou[]  = { 4, 2, 3, 1, 5 };	// 合
 
 		for (int i = 0, count = to.length; i < count; i ++) {
-
+			v = new MyVolume();
+			v.setVolume(to[i], syou[i], gou[i]);
+			v = v.add(v);
 		}
 
 
