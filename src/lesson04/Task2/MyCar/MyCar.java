c9771b48b8eec318dfd4542f754a573034dcc559
hayashin28 <hayashin.28@gmail.com>
2017-06-22 19:18:29 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson04/Task2/MyCar/MyCar.java
@@ -0,0 +1,22 @@
+package lesson04.Task2.MyCar;
+
+public class MyCar {
+
+	private String	 carName;	// 車名を記憶する変数
+	private int		 hosePower;	// 馬力を記憶する変数
+	private int		 maxSpeed;	// 最高速度を記憶する変数
+
+
+	//MyCarオブジェクトに値を設定するメソッド
+	public void setCar(String name, int power, int speed) {}
+
+	//車名を参照するメソッド
+	public String getCarName() { return carName; }
+
+	//馬力を参照するメソッド
+	public int getHosePower() { return hosePower; }
+
+	//最高速度を参照するメソッド
+	public int getMaxSpeed() { return maxSpeed; }
+
+}
