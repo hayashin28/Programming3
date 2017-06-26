c89130cc1dfc97b3a8056e40da90be9483a9148e
hayashin28 <hayashin.28@gmail.com>
2017-06-26 18:43:48 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson04/Task2/MyCarView.java
+++ src/lesson04/Task2/MyCarView.java
@@ -1,19 +1,47 @@
 package lesson04.Task2;
 
 import java.applet.Applet;
+import java.awt.Button;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import lesson04.Task2.MyCar.MyCar;
 
 public class MyCarView extends Applet {
 
 	private MyCar car;
+	private Button  btnName, btnPower, btnSpeed;
 
 
 	@Override
 	public void init() {
 		car = new MyCar();
 		car.setCar("ポルシェ911カレラ", 5900, 315);
+
+		btnName  = new Button("車名");	add(btnName);
+		btnPower = new Button("馬力");	add(btnPower);
+		btnSpeed = new Button("速度");	add(btnSpeed);
+
+
+		btnName.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+
+		btnPower.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+
+
+		btnSpeed.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 	}
 
 	@Override
