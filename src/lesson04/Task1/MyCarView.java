b356e362a173f8e45fd4bf57c91e79909534d115
hayashin28 <hayashin.28@gmail.com>
2017-06-26 20:00:48 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson04/Task1/MyCarView.java
+++ src/lesson04/Task1/MyCarView.java
@@ -6,8 +6,9 @@ import java.awt.Graphics;
 import lesson04.Task1.MyCar.MyCar;
 
 /**
- *
- * @author
+ * 第4回 最終課題1
+ * 車名，馬力，最高速度を設定後，これらの表示
+ * @author 林宏典（15T055）
  *
  */
 public class MyCarView extends Applet{
--- src/lesson04/Task2/MyCarView.java
+++ src/lesson04/Task2/MyCarView.java
@@ -8,30 +8,42 @@ import java.awt.event.MouseEvent;
 
 import lesson04.Task2.MyCar.MyCar;
 
+/**
+ * 第4回 最終課題2
+ * 車名，馬力，最高速度を設定後，これらの表示
+ * @author 林宏典（15T055）
+ *
+ */
 public class MyCarView extends Applet {
 
 	private MyCar car;
 	private Button  btnName, btnPower, btnSpeed;
+	// ボタン判定フラグ
 	private int flg = 0;
 
 	@Override
 	public void init() {
 		car = new MyCar();
 		car.setCar("ポルシェ911カレラ", 5900, 315);
-
+		// ボタンをインスタンス化しアプレットに登録
 		btnName  = new Button("車名");	add(btnName);
 		btnPower = new Button("馬力");	add(btnPower);
 		btnSpeed = new Button("速度");	add(btnSpeed);
 
-
+		/**
+		 *  無名クラスにてボタン押下時の動きを登録
+		 */
+		// 車名ボタン押下時の処理
 		btnName.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				flg = 1;
+				// 描画処理を再呼び出し
 				repaint();
 			}
 		});
 
+		// 馬力ボタン押下時の処理
 		btnPower.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -40,7 +52,7 @@ public class MyCarView extends Applet {
 			}
 		});
 
-
+		// 速度ボタン押下時の処理
 		btnSpeed.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
