ecc73e0ac9b5db63fa168d52b93451f9515f672e
hayashin28 <hayashin.28@gmail.com>
2017-06-27 08:35:32 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson05/Person.java
+++ src/lesson05/Person.java
@@ -1,5 +1,33 @@
 package lesson05;
 
-public class Person {
+import java.applet.Applet;
+import java.awt.Graphics;
 
+import lesson05.lec0501util.PointCardAccount;
+
+/**
+ * 第5回 最終課題1
+ *
+ * @author 林宏典（15T055）
+ *
+ */
+public class Person extends Applet {
+
+		private static final long serialVersionUID = 1L;
+
+		private PointCardAccount account;
+
+		@Override
+		public void init() {
+			// ポイントカードアカウントクラスをインスタンス化
+			account = new PointCardAccount();
+			account.initPointCardAccount("阿笠田奈", 1234, 10000);
+		}
+		@Override
+		public void paint(Graphics g) {
+			// 各体積を表示
+			g.drawString("名前："		  + account.getName()
+		+				 "暗証番号："	  + account.getPersonalCode()
+		+				 "残りポイント：" + account.getBalancePoint(), 30, 30);
+		}
 }
