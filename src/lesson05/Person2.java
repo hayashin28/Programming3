701829a031ab2c107dfb8cd1fc4f5d9d33274d5e
hayashin28 <hayashin.28@gmail.com>
2017-07-06 08:38:12 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson05/Person2.java
+++ src/lesson05/Person2.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.text.NumberFormat;
 
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
@@ -123,7 +124,11 @@ public class Person2 extends JFrame implements ActionListener {
 		 * @return
 		 */
 		private JFormattedTextField getTxtPersonalCode() {
-			if (txtPersonalCode == null) txtPersonalCode = new JFormattedTextField();
+			if (txtPersonalCode == null) {
+				txtPersonalCode = new JFormattedTextField();
+				// 初期設定
+				NumberFormat nf = NumberFormat.getNumberInstance();
+			}
 			return txtPersonalCode;
 		}
 		
