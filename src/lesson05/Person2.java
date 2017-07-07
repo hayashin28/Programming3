8cd1169af92e89402fe343a0cae6ace66a8461c1
hayashin28 <hayashin.28@gmail.com>
2017-07-07 16:30:29 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson05/Person2.java
+++ src/lesson05/Person2.java
@@ -5,14 +5,12 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.text.NumberFormat;
 
-import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import lesson05.lec0501util.PointCardAccount;
 
@@ -23,134 +21,130 @@ import lesson05.lec0501util.PointCardAccount;
  *
  */
 public class Person2 extends JFrame implements ActionListener {
-	
-		private static final long serialVersionUID = 1L;
-		JPanel panel;
-		GridBagLayout grid;
-		JLabel lblPersonalCode;
-		JFormattedTextField txtPersonalCode;
-	
 
-		// コンストラクタでフレームを初期化
-		public Person2() {
-			
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setTitle("JFrameを使ってフレームを表示");
-			setSize(530, 100);
-			
-			getPanel().setLayout(getGrid());
-			
-			GridBagConstraints gbc = new GridBagConstraints();
-			
-			gbc.gridx = 0;
-			gbc.gridy = 0;
-//			gbc.gridwidth  = 1
-//			gbc.gridheight = 2;	// 縦２つ分
-			getLblPersonalCode().setText("暗証番号");
-			getGrid().setConstraints(getLblPersonalCode(), gbc);			
-			getPanel().add(getLblPersonalCode());
-			
-			gbc.gridx = 1;
-			getGrid().setConstraints(getTxtPersonalCode(), gbc);
-			getPanel().add(getTxtPersonalCode());
-				
-				
-			
-			
-			
-			
-			
-			
-			
-			getTxtPersonalCode().addKeyListener(new KeyAdapter() {
-				public void keyPressed(KeyEvent e) {
-					
-					try {
-						if (!(e.getKeyCode()==8 || e.getKeyCode()==37 || e.getKeyCode()==39 || e.getKeyCode()==127)) {
-							Integer.parseInt(String.valueOf(e.getKeyChar()));
-						}
-					} catch (NumberFormatException ex) {
-						e.consume();
-					}
-					
-					
-					
-					
-					
-					
+	private static final long serialVersionUID = 1L;
+	JPanel panel;
+	GridBagLayout grid;
+	JLabel lblPersonalCode;
+	JTextField txtPersonalCode;
+
+
+	// コンストラクタでフレームを初期化
+	public Person2() {
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("JFrameを使ってフレームを表示");
+		setSize(530, 100);
+
+		getPanel().setLayout(getGrid());
+
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		//			gbc.gridwidth  = 1
+		//			gbc.gridheight = 2;	// 縦２つ分
+		getLblPersonalCode().setText("暗証番号");
+		getGrid().setConstraints(getLblPersonalCode(), gbc);			
+		getPanel().add(getLblPersonalCode());
+
+		gbc.gridx = 1;
+		getGrid().setConstraints(getTxtPersonalCode(), gbc);
+		getPanel().add(getTxtPersonalCode());
+
+		
+		
+		
+		getTxtPersonalCode().addActionListener(this);
+
+
+		
+
+
+
+
+
+
+
+
+
+		getContentPane().add(getPanel(), BorderLayout.CENTER);
+		setVisible(true);
+	}
+
+
+
+	/**
+	 * 
+	 * @return
+	 */
+	private JPanel getPanel() {
+		if (panel == null) panel = new JPanel();
+		return panel;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private GridBagLayout getGrid() {
+		if (grid == null) grid = new GridBagLayout();
+		return grid;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private JLabel getLblPersonalCode() {
+		if (lblPersonalCode == null) lblPersonalCode = new JLabel();
+		return lblPersonalCode;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private JTextField getTxtPersonalCode() {
+		if (txtPersonalCode == null) txtPersonalCode = new JTextField("", 4);				
+		return txtPersonalCode;
+	}
+
+
+
+	private static PointCardAccount account;
+
+	public static void main(String[] args) {
+
+		// JFrameクラスのインスタンスを生成
+		new Person2();
+
+
+
+
+		// ポイントカードアカウントクラスをインスタンス化
+		account = new PointCardAccount();
+		account.initPointCardAccount("阿笠田奈", 1234, 10000);
+	}
+
+
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == getTxtPersonalCode()) {
+			try {
+				if (account.getPersonalCode() == Integer.parseInt(getTxtPersonalCode().getText())) {
+					return;													
 				}
-			});
-			
-			
-			
-			
-			getContentPane().add(getPanel(), BorderLayout.CENTER);
-			setVisible(true);
-			
-			
-		}
-		
-		
-		
-		/**
-		 * 
-		 * @return
-		 */
-		private JPanel getPanel() {
-			if (panel == null) panel = new JPanel();
-			return panel;
-		}
-		
-		/**
-		 * 
-		 * @return
-		 */
-		private GridBagLayout getGrid() {
-			if (grid == null) grid = new GridBagLayout();
-			return grid;
-		}
-		
-		/**
-		 * 
-		 * @return
-		 */
-		private JLabel getLblPersonalCode() {
-			if (lblPersonalCode == null) lblPersonalCode = new JLabel();
-			return lblPersonalCode;
-		}
-		
-		/**
-		 * 
-		 * @return
-		 */
-		private JFormattedTextField getTxtPersonalCode() {
-			if (txtPersonalCode == null) {
-				txtPersonalCode = new JFormattedTextField();
-				// 初期設定
-				NumberFormat nf = NumberFormat.getNumberInstance();
+				else {
+					//●タイトルを付けたメッセージ出力
+					JOptionPane.showMessageDialog(null, "本文", "タイトル", JOptionPane.INFORMATION_MESSAGE);	
+				}
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "本文", "タイトル", JOptionPane.INFORMATION_MESSAGE);	
 			}
-			return txtPersonalCode;
+		} 
+		else {
+			
 		}
-		
-		
-		
-		private static PointCardAccount account;
-
-		public static void main(String[] args) {
-
-			// JFrameクラスのインスタンスを生成
-			new Person2();
-
-
-
-
-			// ポイントカードアカウントクラスをインスタンス化
-			account = new PointCardAccount();
-			account.initPointCardAccount("阿笠田奈", 1234, 10000);
-		}
-		
-		
-		
-		public void actionPerformed(ActionEvent e) {
-		}
+	}
 }
