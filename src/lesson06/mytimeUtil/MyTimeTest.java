20fd185379cfdb504a82b05d8185b32d0dcdf9c6
hayashin28 <hayashin.28@gmail.com>
2017-07-09 17:33:01 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson06/mytimeUtil/MyTimeTest.java
+++ src/lesson06/mytimeUtil/MyTimeTest.java
@@ -14,35 +14,65 @@ public class MyTimeTest {
 	public void testSetTime() {
 		// 1時間2分3秒が正しく設定されるかテストする
 		MyTime t1 = new MyTime();   // (1)
-		t1.setTime( 1, 2, 3);       // (2)
-		assertEquals( "時間は1のはず", 1, t1.getHour());  // (3)
-		assertEquals( "分は2のはず", 2, t1.getMinute());  // (4)
-		assertEquals( "秒は3のはず", 3, t1.getSecond());  // (5)
+		t1.setTime(1, 2, 3);       // (2)
+		assertEquals( "時間は1のはず", 1, t1.getHour());   // (3)
+		assertEquals( "分は2のはず",   2, t1.getMinute()); // (4)
+		assertEquals( "秒は3のはず",   3, t1.getSecond()); // (5)
 		
 		// (1*3600+2*60+3)秒が正しく設定されるかテストする
-		t1.setTime( 0, 0, (3600+2*60+3));  // (6)
-		assertEquals( "時間は1のはず", 1, t1.getHour());  // (7)
-		assertEquals( "分は2のはず", 2, t1.getMinute());  // (8)
-		assertEquals( "秒は3のはず", 3, t1.getSecond());  // (9)
+		t1.setTime(0, 0, (3600+2*60+3));  // (6)
+		assertEquals("時間は1のはず",1, t1.getHour());     // (7)
+		assertEquals("分は2のはず",  2, t1.getMinute());   // (8)
+		assertEquals("秒は3のはず",  3, t1.getSecond());   // (9)
 	}
 	
 	@Test
+	/**
+	 * 'mytimeUtil.MyTime.getHour()' のためのテスト・メソッド
+	 */
 	public void testGetHour() {
-		fail("まだ実装されていません");
+		// 1時間2分3秒を設定して，時間の値をテストする
+		MyTime t1 = new MyTime();
+		t1.setTime(1, 2, 3);
+		assertEquals("時間は1のはず", 1, t1.getHour());	
 	}
 
 	@Test
+	/**
+	 * 'mytimeUtil.MyTime.getMinute()' のためのテスト・メソッド
+	 */
 	public void testGetMinute() {
-		fail("まだ実装されていません");
+		// 1時間2分3秒を設定して，分の値をテストする
+		MyTime t1 = new MyTime();
+		t1.setTime(1, 2, 3);
+		assertEquals("分は2のはず", 2, t1.getMinute());
 	}
 
 	@Test
+	/**
+	 * 'mytimeUtil.MyTime.getSecond()' のためのテスト・メソッド
+	 */
 	public void testGetSecond() {
-		fail("まだ実装されていません");
+		// 1時間2分3秒を設定して，秒の値をテストする
+		MyTime t1 = new MyTime();
+		t1.setTime(1, 2, 3);
+		assertEquals("秒は3のはず", 3, t1.getSecond());		fail("まだ実装されていません");
 	}
 
 	@Test
+	/**
+	 * 'mytimeUtil.MyTime.add(MyTime)' のためのテスト・メソッド
+	 */
 	public void testAdd() {
-		fail("まだ実装されていません");
-	}
+		// 1h2m3s + 2h59m40s = 4h1m43s となっているかをテストする
+		MyTime t1 = new MyTime();
+		t1.setTime(1, 2, 3);
+		MyTime t2 = new MyTime();
+		t2.setTime(2, 59, 40);
+		MyTime actual;
+		actual = t1.add( t2);
+		
+		assertEquals("時間は4のはず", 4, actual.getHour());
+		assertEquals("分は1のはず",   1, actual.getMinute());
+		assertEquals("秒は43のはず", 43, actual.getSecond());		}
 }
