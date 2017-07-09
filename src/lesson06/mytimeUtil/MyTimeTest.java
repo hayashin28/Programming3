ba787fb6e2ef0312d80d42ea98f5ac4cdb0fb83e
hayashin28 <hayashin.28@gmail.com>
2017-07-09 16:52:44 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson06/mytimeUtil/MyTimeTest.java
@@ -0,0 +1,34 @@
+package lesson06.mytimeUtil;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTimeTest {
+
+	@Test
+	public void testSetTime() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetHour() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetMinute() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetSecond() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("まだ実装されていません");
+	}
+
+}
