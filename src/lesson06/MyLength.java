8a717343a2a428e198793457d85c6594f17e86ab
hayashin28 <hayashin.28@gmail.com>
2017-07-10 19:20:22 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson06/MyLength.java
+++ src/lesson06/MyLength.java
@@ -1,18 +1,37 @@
 package lesson06;
+import junit.framework.TestCase;
 
-import java.applet.Applet;
-import java.awt.Graphics;
+public class MyLength {
 
-import lesson06.mytimeUtil.MyTime;
+	/*
+	 * 'mylengthUtil.MyLength.setLength(int, int)' のためのテスト・メソッド
+	 */
+	public void setLength() {
 
-public class Lec0401 extends Applet{
+		//1フィート2インチが正しく設定されているかテストする
 
-	private static final long serialVersionUID = 1L;
+		//0フィート14(=12+2)インチが正しく設定されているかテストする
+	}
 
-	public void paint(Graphics g){
-		MyTime t= new MyTime();
-		t.setTime( 10, 20, 30);
-		g.drawString("H:"+t.getHour()+"M:"+t.getMinute()+"S:"+t.getSecond(),30, 30);
-		   }
+	/*
+	 * 'mylengthUtil.MyLength.getFeet()' のためのテスト・メソッド
+	 */
+	public void getFeet() {
+		//1フィート2インチが正しく設定されているかテストする
+	}
+
+	/*
+	 * 'mylengthUtil.MyLength.getInch()' のためのテスト・メソッド
+	 */
+	public void testGetInch() {
+		//1フィート2インチが正しく設定されているかテストする
+	}
+
+	/*
+	 * 'mylengthUtil.MyLength.add(MyLength)' のためのテスト・メソッド
+	 */
+	public void add() {
+		// 1feet2inch + 1feet11inch = 3feet1inch となっているかをテストする
+	}
 
 }
