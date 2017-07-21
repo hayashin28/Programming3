26c3aa14c68507b0c3856b9977ad3e1c37259b89
Hayashin28 <hayashin.28@gmail.com>
2017-07-21 09:50:46 +0900

    Add files via upload

---
+++ src/lesson06/MyLength.java
@@ -0,0 +1,28 @@
+package lesson06.mylengthUtil;
+
+
+//時間を扱うクラスMyTime
+public class MyLength {		
+	// フィールド
+
+    private int feet = 0; // フィートの値を記憶
+    private int inch = 0; // インチの値を記憶
+
+	// メソッド
+
+    // 長さオブジェクトに値を設定
+    public void setLength(int ft, int in) {
+    	inch = (inch + in) % 12;
+    	feet += ft + in / 12;
+    }
+    // フィートを参照
+    public int getFeet() { return feet; }
+    //インチを参照
+    public int getInch() { return inch; }
+    // MyLengthオブジェクト同士の加算
+    public MyLength add(MyLength x) {
+    	MyLength len = new MyLength();
+    	len.setLength(feet + x.getFeet(), inch + x.getInch());
+    	return len;
+    }
+}
