8e995098b4d0cc63fae55c0ceebab37818a90c0f
hayashin28 <hayashin.28@gmail.com>
2017-07-25 18:42:01 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson08/MyCarView.java
@@ -0,0 +1,23 @@
+package lesson08;
+
+import java.applet.Applet;
+
+import lesson08.lec0801util.MyCar;
+
+public class MyCarView extends Applet {
+
+	private static final long serialVersionUID = 1L;
+	
+	MyCar [] car = { new MyCar("フェラーリ", 490, 315),
+					 new MyCar("ランボルギーニ",510),
+					 new MyCar("ポルシェ", 310, 300),
+					 new MyCar("カローラ",100) 
+					};
+
+
+	
+	
+	
+	
+
+}
