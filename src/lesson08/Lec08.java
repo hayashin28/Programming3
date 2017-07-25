4eb659ba598030133e0ee75f1ac7a5e6717e0ef5
hayashin28 <hayashin.28@gmail.com>
2017-07-25 16:22:53 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson08/Lec08.java
@@ -0,0 +1,14 @@
+package lesson08;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Lec08 extends Applet{
+	private static final long serialVersionUID = 1L;
+
+	public void paint(Graphics g){
+		//コンストラクタを用いたクラスMyTimeのインスタンスtの生成
+		MyTime t= new MyTime(10,20,30);
+		g.drawString("H:"+t.getHour()+" M:"+t.getMinute()+" S:"+t.getSecond(), 30, 30);
+	}
+}
