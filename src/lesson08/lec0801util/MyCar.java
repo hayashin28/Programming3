5342f0de797712abf9332c28b2142daaa716022d
hayashin28 <hayashin.28@gmail.com>
2017-07-25 17:46:40 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson08/lec0801util/MyCar.java
+++ src/lesson08/lec0801util/MyCar.java
@@ -1,5 +1,28 @@
 package lesson08.lec0801util;
 
-public class MyCar {
 
+public class MyCar {
+	private String 	carName; 	// 車名を保持するフィールド変数
+	private int 	hosePower; 	// 最大出力を保持するフィールド変数
+	private int 	maxSpeed; 	// 最高速度を保持するフィールド変数
+
+	/**
+	 * コンストラクタの仕様
+	 * MyCarクラスのオブジェクトの3つのフィールドを引数の値で初期化します．
+	 * @param carName
+	 * @param hosePower
+	 * @param maxSpeed
+	 */
+	public MyCar(String carName, int hosePower, int maxSpeed) {
+		this.carName	= carName;
+		this.hosePower	= hosePower;
+		this.maxSpeed	= maxSpeed;
+	}
+
+	// 車名を返す
+	public String getCarName() { return carName; }
+	// 最大出力を返す
+	public int getHosePower() { return hosePower; }
+	// 最高速度を返す 
+	public int getMaxSpeed() { return maxSpeed; }
 }
