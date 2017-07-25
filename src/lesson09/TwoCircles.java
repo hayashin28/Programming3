aa3be02d8779be030356b24fdc957f33a99c8c4b
hayashin28 <hayashin.28@gmail.com>
2017-07-26 08:48:38 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson09/TwoCircles.java
@@ -0,0 +1,15 @@
+package lesson09;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class TwoCircles extends Applet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void paint(Graphics g) {
+		
+		
+		
+	}
+}
