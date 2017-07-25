6a94725a964ca448f15d66ea2785948b1fc05b4d
hayashin28 <hayashin.28@gmail.com>
2017-07-25 19:32:07 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

--- src/lesson08/MyCarView.java
+++ src/lesson08/MyCarView.java
@@ -1,6 +1,7 @@
 package lesson08;
 
 import java.applet.Applet;
+import java.awt.Graphics;
 
 import lesson08.lec0801util.MyCar;
 
@@ -14,10 +15,18 @@ public class MyCarView extends Applet {
 					 new MyCar("カローラ",100) 
 					};
 
-
-	
-	
-	
-	
-
+	@Override
+	public void paint(Graphics g) {
+		MyCar Highspec = null;
+		int max = 0;
+		for (MyCar c : car) {
+			if (max < c.getHosePower()) {
+				Highspec = c;
+				max = c.getHosePower();
+			}
+		}
+		// 車の情報を表示
+		g.drawString("最大馬力の車名：" + Highspec.getCarName()
+		+			 "馬力："		   + Highspec.getHosePower(), 30, 30);
+	}
 }
