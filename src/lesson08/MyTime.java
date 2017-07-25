16c6b48bd947ad730a385bff0bf03e8da806f283
hayashin28 <hayashin.28@gmail.com>
2017-07-25 16:14:27 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson08/MyTime.java
@@ -0,0 +1,26 @@
+package lesson08;
+
+public class MyTime {
+	//フィールド
+	//時間を記憶する
+	private int hour;
+	//分を記憶する
+	private int minute;
+	//秒を記憶する
+	private int second;
+	
+	//コンストラクタ
+	public MyTime( int h, int m, int s){
+		second = s % 60;
+		minute = ( m + s/60)%60;
+		hour =  h + ( m + s/60)/60;
+	}
+
+	//メソッド
+	//時間を返す
+	public int getHour() { return hour; }
+	//分を返す
+	public int getMinute() { return minute; }
+	//秒を返す
+	public int getSecond() { return second; }
+}
