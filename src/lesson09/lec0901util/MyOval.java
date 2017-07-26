45fa2808a6a2b12314a16ebe12dcb35811fa2e1c
hayashin28 <hayashin.28@gmail.com>
2017-07-26 10:11:37 +0900

    Signed-off-by: hayashin28 <hayashin.28@gmail.com>

---
+++ src/lesson09/lec0901util/MyOval.java
@@ -0,0 +1,13 @@
+package lesson09.lec0901util;
+
+
+
+public class MyOval {
+
+	private	int xPosition; 	// 円の始点のx座標を保持するフィールド変数
+	private	int yPosition; 	// 円の始点のy座標を保持するフィールド変数
+	private	int radius; 	// 円の半径を保持するフィールド変数
+	private	int colorRed; 	// 円の色（赤の要素）を保持するフィールド変数
+	private	int colorGreen; // 円の色（緑の要素）を保持するフィールド変数
+	private	int colorBlue; 	// 円の色（青の要素）を保持するフィールド変数
+}
